c1b718075edd0cd9e05cf67b28001b212b6cc328
Aviral Varshney <aviralvarshney07@gmail.com>
2026-01-05 21:03:16 +0530

    Write File

--- File System/writeFile.js
+++ File System/writeFile.js
@@ -1,5 +1,20 @@
+// const fs = require("fs");
+// fs.writeFile("Hello.txt", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", (err)=>{
+//     if(err)console.log(err);
+//     else console.log("Success! ");
+// })
+
 const fs = require("fs");
-fs.writeFile("Hello.txt", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", (err)=>{
+setTimeout(()=>{    //this is async..time lerha threadpool mei jaa
+    console.log("B/f"); //5th o/p
+},2000);
+console.log("holaaaaaaaa"); //1st o/p
+const save = fs.writeFile("abcdefghij.txt", "ASync", (err)=>{   //ab kyuki ye async hai to time lerha to threadpool mei gaya
     if(err)console.log(err);
-    else console.log("Success! ");
-})
+    else console.log("done"); //4th o/p
+});
+console.log(save);//undefined 2nd o/p
+console.log("holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"); //3rd o/p
+setTimeout(()=>{
+    console.log("A/f"); //6th o/p
+},4000);
